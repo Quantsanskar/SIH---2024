4328218f5d52d5631b65fd1841acc7f87d032f2d
Sanskar Bhardwaj <sanskarsrdav@gmail.com>
2024-08-30 18:26:03 +0530

    ...

--- project/home/urls.py
+++ project/home/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path("", views.index, name="home"),
     path("register", views.register, name="register"),
